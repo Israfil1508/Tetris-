7ecba7e65ab36edbe16f07efb6ceb1c61aac6161
Israfil1508 <bsse1508@iit.du.ac.bd>
2025-03-19 00:14:35 +0600

    Add files via upload

---
+++ DrawText.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include<vector>
+#include "Global.hpp"
+#include<string>
+using namespace std ;
+void draw_text(unsigned short i_x, unsigned short i_y, const std::string& i_text, sf::RenderWindow& i_window);
---
+++ GetTetromino.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include<vector>
+#include "Global.hpp"
+#include "GetWalkickData.hpp" 
+
+std::vector<Position> get_tetromino(unsigned char i_shape, unsigned char i_x, unsigned char i_y);
